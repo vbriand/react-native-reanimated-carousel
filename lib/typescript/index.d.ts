149e62af0e0c2299c454e1ea9ccd9cce57ed751e
Valentin Briand <678530+vbriand@users.noreply.github.com>
2024-04-12 00:13:02 +0200

    fix: workaround to use as dependency without publishing

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,5 @@
+import Carousel from "./components/Carousel";
+export type { TCarouselProps, ICarouselInstance, IComputedDirectionTypes, CarouselRenderItem, } from "./types";
+export type { TAnimationStyle } from "./components/BaseLayout";
+export type { ILayoutConfig } from "./layouts/stack";
+export default Carousel;
